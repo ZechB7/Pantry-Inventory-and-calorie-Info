e9d25dd911a4c7b44e3f094a6c35c34ff3aa762d
ZechB7 <117792233+ZechB7@users.noreply.github.com>
2023-01-12 14:35:10 -0600

    added and tested api key

--- Assets/javascripts/script.js
+++ Assets/javascripts/script.js
@@ -1,10 +1,29 @@
 // input for recipe and ingredients get
-var query = 'inputSearch'
+var query = "text"
+// document.getElementById("search-bar").value
+// var searchFormEl = document.querySelector('#search-form');
+
+// function handleSearchFormSubmit(event) {
+//   event.preventDefault();
+
+//   var searchInputVal = document.querySelector('#search-input').value;
+//   if (!searchInputVal) {
+//     console.error('You need a search input value!');
+//     return;
+//   }
+
+//   var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+//   location.assign(queryString);
+// }
+
+// searchFormEl.addEventListener('click', handleSearchFormSubmit);
+
 
 // recipe and ingredients get
 $.ajax({
     method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/recipe?query=' + query,
+    url: 'https://api.api-ninjas.com/v1/recipe?query=' + 'taco',
     headers: { 'X-Api-Key': 'vxibSWpyKxfvY5Ra3REMUA==gGksQsKJpEJb8KeT'},
     contentType: 'application/json',
     success: function(result) {
@@ -16,9 +35,9 @@ $.ajax({
 });
 
 // in html needs div class of widget
-$( function() {
-  $( ".widget input[type=submit], .widget a, .widget button" ).button();
-  $( "button, input, a" ).on( "click", function( event ) {
-    event.preventDefault();
-  } );
-} );
+// $( function() {
+//   $( ".widget input[type=submit], .widget a, .widget button" ).button();
+//   $( "button, input, a" ).on( "click", function( event ) {
+//     event.preventDefault();
+//   } );
+// } );
