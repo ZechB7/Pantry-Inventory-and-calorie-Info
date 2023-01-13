4cd1152adde1091b1f9ec8b64cbafe87c692ab66
connorbodin <connor.e.bodin@gmail.com>
2023-01-13 16:39:43 -0600

    commit

--- Assets/javascripts/results.js
+++ Assets/javascripts/results.js
@@ -14,3 +14,40 @@ result.forEach((recipe) => {
   // resultsContainer.appendChild(recipeIngredients);
   console.log(recipeIngredients);
 });
+
+$("#search-button").click(function (e) {
+  e.preventDefault();
+  var recipeSearch = $("#site-search").val();
+
+  fetch("https://api.api-ninjas.com/v1/recipe?query=" + recipeSearch, {
+    method: "GET",
+    headers: { "X-Api-Key": "vxibSWpyKxfvY5Ra3REMUA==gGksQsKJpEJb8KeT" },
+    contentType: "application/json",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((result) => {
+      console.log(result);
+      // Store data
+      localStorage.setItem("recipe", JSON.stringify(result));
+      // do something with the result here
+      // window.location.href = "./results.html";
+    })
+    .catch((error) => {
+      console.error("Error: ", error);
+      // Retrieve data
+      const recipe = JSON.parse(localStorage.getItem("recipe")) || [];
+      recipe.push(result);
+
+      if (!recipe) {
+        console.error("No data in local storage");
+      } else {
+        console.log(recipe);
+        // handle the error here
+      }
+    });
+});
