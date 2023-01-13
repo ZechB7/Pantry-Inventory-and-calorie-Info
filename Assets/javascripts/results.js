cc0be3d4b00f28810f7cbfa8f13f7e4b25b1a988
ZechB7 <117792233+ZechB7@users.noreply.github.com>
2023-01-13 13:09:00 -0600

    enter maybe

--- Assets/javascripts/results.js
+++ Assets/javascripts/results.js
@@ -8,3 +8,42 @@ result.forEach((recipe) => {
   recipeContainer.appendChild(recipeTitle);
   resultsContainer.appendChild(recipeContainer);
 });
+
+document.getElementById('site-search').addEventListener('keypress', function (e) {
+  if (e.key === 'enter'){
+  e.preventDefault();
+
+  var recipeSearch = $("#site-search").val();
+
+  fetch("https://api.api-ninjas.com/v1/recipe?query=" + recipeSearch, {
+    method: "GET",
+    headers: { "X-Api-Key": "vxibSWpyKxfvY5Ra3REMUA==gGksQsKJpEJb8KeT" },
+    contentType: "application/json",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((result) => {
+      console.log(result);
+      // Store data
+      localStorage.setItem("recipe", JSON.stringify(result));
+      // do something with the result here
+      // window.location.href = "../../results.html";
+    })
+    .catch((error) => {
+      console.error("Error: ", error);
+      // Retrieve data
+      const recipe = JSON.parse(localStorage.getItem("recipe")) || [];
+      recipe.push(result);
+
+      if (!recipe) {
+        console.error("No data in local storage");
+      } else {
+        console.log(recipe);
+        // handle the error here
+      }
+    });
+}});
--- Assets/javascripts/script.js
+++ Assets/javascripts/script.js
@@ -1,9 +1,10 @@
 //Recipe api ninja key = vxibSWpyKxfvY5Ra3REMUA==gGksQsKJpEJb8KeT
 
 //button function
+
 $("#search-button").click(function (e) {
-  e.preventDefault();
-  var recipeSearch = $("#search-bar").val();
+    e.preventDefault();
+    var recipeSearch = $("#search-bar").val();
 
   fetch("https://api.api-ninjas.com/v1/recipe?query=" + recipeSearch, {
     method: "GET",
