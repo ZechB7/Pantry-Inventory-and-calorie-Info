624775d3228e76281be25b0305fbe63fce46b90d
connorbodin <connor.e.bodin@gmail.com>
2023-01-14 15:02:05 -0600

    Added rounded buttons -- working on collapse capabilities

--- Assets/javascripts/results.js
+++ Assets/javascripts/results.js
@@ -10,34 +10,60 @@ result.forEach((recipe, i) => {
   if (i < 6) {
     recipeContainer.className =
       "bg-white rounded overflow-hidden shadow-md relative";
-    
+
+// Working on the below collapse capabilities 
+
+  // let collapseDiv = document.createElement("div")
+  // if (collapseDiv) {
+  //   collapseDiv.setAttribute("class", "collapse border border-base-300 bg-base-100 rounded-box");
+  // }
+  // collapseDiv.tabIndex = 0
+
   
+  // let collapseTitle = document.createElement("div")
+  // collapseTitle.className = "collapse-title text-xl font-medium";
+  // collapseTitle.innerHTML = "Recipe Name: "+ recipe.title + "<br>" + "<br>" + "Ingredients: " + "<br>" + "<br>" + ingredientsList;
+
+  // let collapseContent = document.createElement("div");
+  // collapseContent.className = "collapse-content";
+
+  // let instructions = document.createElement("p")
+  // instructions.innerHTML = recipe.instructions
+
+  // recipeContainer.appendChild(collapseDiv);
+  // collapseDiv.appendChild(collapseTitle);
+  // collapseDiv.appendChild(collapseContent);
+  // collapseContent.appendChild(instructions);
+  // resultsContainer.appendChild(collapseDiv);
+
+  var recipeTit = recipe.title
+  recipeTit.className = "text-4xl"
 
   let recipeTitle = document.createElement("h1");
-
+  recipeTitle.className = 'text-xl'
   let recipeIngredients = document.createElement("p");
   recipeIngredients.className = "text-align-left";
   let recipeButton = document.createElement("button");
-  recipeButton.className = " px-4 py-2.5 bg-blue-500 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out collapsible";
+  recipeButton.className = " w-full h-full px-4 py-2.5 text-start bg-blue-500 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out collapsible";
   recipeIngredients.className = "py-2";
-  let ingredientsList = recipe.ingredients.replaceAll("|", "<li>");
+  let ingredientsList = recipe.ingredients.replaceAll("|", "<br>");
   
   console.log(ingredientsList);
-  let collapseDiv = document.createElement("div")
+  
 
   let recipeInstruction = document.createElement("p");
   recipeInstruction.className = "py-2";
   let recipeServing = document.createElement("p");
-  recipeButton.innerHTML = recipe.title + " Recipe" + "<br>" + "<br>" + ingredientsList;
+  recipeButton.innerHTML = recipeTit + " Recipe" + "<br>" + "<br>" + "Ingredients: " + "<br>" + "<br>" + ingredientsList;
 
-  recipeIngredients.innerHTML = "Ingredients: <br>" + ingredientsList;
+  // recipeIngredients.innerHTML = "Ingredients: <br>" + "Ingredients: " +  + ingredientsList;
 
   recipeInstruction.innerHTML = "Recipe: <br>" + recipe.instructions;
   recipeServing.innerHTML = "Serving: " + recipe.servings;
   recipeContainer.appendChild(recipeButton);
-  recipeContainer.appendChild(recipeIngredients);
-  recipeContainer.appendChild(recipeInstruction);
-  recipeContainer.appendChild(recipeServing);
+  // recipeContainer.appendChild(recipeIngredients);
+  // recipeContainer.appendChild(recipeInstruction);
+  // recipeContainer.appendChild(recipeServing);
   resultsContainer.appendChild(recipeContainer);
   // resultsContainer.appendChild(recipeIngredients);
   console.log(recipeIngredients);
