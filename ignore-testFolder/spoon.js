a1e177d1753db890c5083e22496eff0fe82c7f36
ZechB7 <117792233+ZechB7@users.noreply.github.com>
2023-01-17 14:52:36 -0600

    tested third api and adding semantics

--- ignore-testFolder/spoon.js
+++ ignore-testFolder/spoon.js
@@ -1,3 +1,8 @@
+// response.setHeader("Access-Control-Allow-Origin", "https://api.spoonacular.com/recipes/findByIngredients?ingredients=");
+// response.setHeader("Access-Control-Allow-Credentials", "true");
+// response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+// response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
@@ -9,20 +14,40 @@ searchBtn.addEventListener('click', getMealList);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
-var apiKey = '43eee7e0350b48fa88589ac4a1b29549'
 // get meal list that matches with the ingredients
-function getMealList(){
+function getMealList() {
     let searchInputTxt = document.getElementById('search-input').value.trim();
-    fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${searchInputTxt}=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-        let html = "";
-        if(data.meals){
-            data.meals.forEach(meal => {
-                html += `
+    // // fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=", {
+    // //   method: "GET",
+    // //   headers: { "X-Api-Key": "43eee7e0350b48fa88589ac4a1b29549" },
+    // //   contentType: "application/json",  
+    // })
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '0036531f0fmshdc93cb2175edd9cp1dd13ejsna1f27e390024',
+            'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+        }
+    };
+
+    fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients='+ searchInputTxt +'&number=5&ignorePantry=true&ranking=1', options)
+
+        // .then(response => response.json())
+        // .then(response => console.log(response))
+        // .catch(err => console.error(err));
+
+        .then(response => response.json())
+        .then(data => {
+
+            console.log(data);
+            let html = "";
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                    html += `
                     <div class = "meal-item" data-id = "${meal.idMeal}">
                         <div class = "meal-img">
-                            <img src = "${meal.strMealThumb}" alt = "food">
+                            <img src = "${meal.img}" alt = "food">
                         </div>
                         <div class = "meal-name">
                             <h3>${meal.strMeal}</h3>
@@ -30,15 +55,15 @@ function getMealList(){
                         </div>
                     </div>
                 `;
-            });
-            mealList.classList.remove('notFound');
-        } else{
-            html = "Sorry, we didn't find any meal!";
-            mealList.classList.add('notFound');
-        }
+                });
+                mealList.classList.remove('notFound');
+            } else {
+                html = "Sorry, we didn't find any meal!";
+                mealList.classList.add('notFound');
+            }
 
-        mealList.innerHTML = html;
-    });
+            mealList.innerHTML = html;
+        });
 }
 
 
