78cc10e83f8a828ae5c5e3d97e6f644334a820f9
ZechB7 <117792233+ZechB7@users.noreply.github.com>
2023-01-17 20:56:51 -0600

    added search on enter key on home page

--- Assets/javascripts/script.js
+++ Assets/javascripts/script.js
@@ -27,3 +27,34 @@ $("#search-button").click(async function (e) {
   // window.location.href = "./results.html";
 
 });
+
+$("#search-bar").keypress(async function (event) {
+  var keycode = event.keyCode ? event.keyCode : event.which;
+  if (keycode === 13) {
+    event.preventDefault();
+    var searchInput = $("#search-bar").val();
+
+  var recipe = await fetchRecipe(searchInput);
+
+  if (recipe.length > 0) {
+    // Store data
+    localStorage.setItem("recipe", JSON.stringify(recipe));
+  } else {
+    console.log("No Search Results");
+    document.getElementById("errorText").innerHTML = "No Search Results";
+  }
+
+  var calories = await fetchCalories(searchInput);
+
+  if (calories.items.length > 0) {
+    localStorage.setItem("calories", JSON.stringify(calories));
+    window.location.href = "./results.html";
+  } else {
+    console.log("No Search Results");
+     setTimeout(() => {
+      document.location.reload();
+    }, 1000); /*---- > We can add this if you want a timeout feature!! */
+  }
+  // window.location.href = "./results.html";
+
+}});
